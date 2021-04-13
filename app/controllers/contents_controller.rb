d5900474b78d09951126e283501f212b1bf4c27a
Victor B S <victor-b.s@hotmail.com>
2021-04-12 21:43:19 -0300

    Adiciona a habilidade de mostrar e de editar conteúdos

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -5,8 +5,42 @@ class ContentsController < ApplicationController
         @contents = current_user.contents
     end
 
+    def show
+        @content = Content.find(params[:id])
+    end
+
     def new
         @content = Content.new
+    end
+
+    def create
+        @content = current_user.contents.build(content_params)
+
+        if @content.save
+            redirect_to contents_path, notice: "Content successfully created"
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @content = Content.find(params[:id])
+    end
+
+    def update
+        @content = Content.find(params[:id])
+
+        if @content.update(content_params)
+            redirect_to contents_path, notice: "Content successfully updated"
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def content_params
+        params.require(:content).permit(:title, :description)
     end
   end
   
