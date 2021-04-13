6b02349af5a8575cc95a6eb393761b2a4c9a7029
Victor B S <victor-b.s@hotmail.com>
2021-04-13 03:00:54 -0300

    Exibe tags na tela home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
-  def index
+  def index  
+    @tags_data = current_user.contents.joins(:tags).group('tags.name').count
   end
 end
